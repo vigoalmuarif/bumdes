f8c5f0a9e2bdc8a8dd7f866e5ea26e62c14d0392
vigoalmuarif <vigoalmuarif@gmail.com>
2021-02-07 04:11:44 +0700

    mengubah file template

--- app/Views/templates/templates.php
+++ app/Views/templates/templates.php
@@ -33,7 +33,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">BUMDes</div>
+                <div class="sidebar-brand-text mx-3">BUMDes</div>hhh
             </a>
 
             <!-- Divider -->
